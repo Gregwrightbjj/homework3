b9dcd3ee22d68a13bf32e7fbf9bf477e1901f5e0
Greg Wright <gregwrightbjj@gmail.com>
2015-05-07 18:00:44 -0400

    clean

---
+++ The Iron Yard Test Runner_files/array.js
@@ -0,0 +1,157 @@
+/*
+  This function should accept a number as a parameter.
+  It should return the value of the number times 2.
+*/
+var doubler = function(number) {
+  return number * 2;
+}
+
+/*
+  This function should accept an array of numbers as a parameter.
+  It should return an array, with each number in the array being doubled.
+*/
+var arrayDoubler = function(array) {
+  var a = [];
+    for(i = 0 ;i < array.length;i++){
+    a.push(doubler(array[i]));
+
+  }return a;
+}
+
+/*
+  This function should accept two numbers as parameters.
+  It should return the first number, divided by the second number.
+*/
+var divide = function(numerator, denominator) {
+  return numerator / denominator;
+
+}
+
+/*
+  This function should accept two parameters: an array of numbers, and another number.
+  It should return an array, with each member of the array being divided by the parameter number.
+*/
+var arrayDivide = function(array, denominator) {
+  var a = [];
+    for (i = 0; i < array.length; i++){
+    a.push(divide(array[i],denominator));
+  }return a;
+}
+
+/*
+  This function should accept two numbers as parameters.
+  It should return the sum of the two numbers.
+*/
+var sum = function(a, b) {
+  return a + b;
+
+}
+
+/*
+  This function should accept an array as a parameter.
+  It should return the sum of all the members in the array.
+*/
+var arraySum = function(array) {
+  var a = 0;
+  for (i = 0; i < array.length; i++){
+    a = sum(array[i], a);
+
+  }return a;
+
+}
+
+/*
+  This function should accept two numbers as parameters.
+  It should return the lower of the two numbers.
+*/
+var minimum = function(a, b) {
+  if(a > b){
+    return b;
+  }else return a;
+
+}
+
+/*
+  This function should accept an array as a parameter.
+  It should return the lowest value in the array.
+  If the array has a length = 0, it should return undefined.
+*/
+var arrayMinimum = function(array) {
+  var a = 0;
+  for (i = 0; i < array.length; i++){
+   a = minimum(array[i], a);
+  }return a;
+
+}
+
+/*
+  This function should accept a string as a parameter.
+  It should return true if the string ends with a period.
+  Otherwise, it should return false.
+*/
+var endsInPeriod = function(string) {
+  var b = string[string.length-1];
+  
+  if (b == "."){
+    return true;
+  }else
+  return false;
+  }
+
+
+/*
+  This functions hould accept an array of strings as a parameter.
+  It should return an array, consisting of only the strings 
+    from the original array which end with a period.
+*/
+var arrayEndsInPeriod = function(array) {
+  var a = [];
+  for(i=0; i < array.length; i++){
+    if (endsInPeriod(array[i])){
+      a.push(array[i]);
+    }
+    
+      
+  }
+return a;
+}
+
+/*
+  This function should accept a number as a parameter.
+  It should return true if the number is a multiple of 3.
+  Otherwise, it should return false.
+*/
+var multiple3 = function(number) {
+  if(number % 3 === 0){
+    return true;
+  }
+  else
+    return false;
+
+  
+}
+
+/*
+  This function should accept an array of numbers as a paramter.
+  It should return an array, consisting of only the numbers
+    from the original array which are multiples of 3.
+*/
+var arrayMultiple3 = function(array) {
+  var a = [];
+    for(i=0; i < array.length; i++){
+    if (multiple3(array[i])){
+      
+    a.push(array[i]);
+    }
+    
+  }
+return a;
+
+}
+
+
+
+
+
+
+
